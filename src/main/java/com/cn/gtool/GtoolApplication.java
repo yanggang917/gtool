357b0a0361000183f7b82a452bf1c64761e2a0a2
yanggang <15805194823@139.com>
2019-12-05 13:21:19 +0800

    Initial commit

---
+++ src/main/java/com/cn/gtool/GtoolApplication.java
@@ -0,0 +1,13 @@
+package com.cn.gtool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GtoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GtoolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cn/gtool/GtoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cn.gtool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GtoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
