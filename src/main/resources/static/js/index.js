19226fdac0155d48625f1d99b0859f699e2404b1
yanggang <15805194823@139.com>
2019-12-24 13:50:10 +0800

    add code

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -1,9 +1,7 @@
 var tab = null;
 var accordion = null;
 var tree = null;
-$(function ()
-{
-
+$(function () {
     var username = getCookie("USERNAME");
     if (username == null){
         loginOut();
@@ -68,7 +66,45 @@ $(function ()
     initUserpasswordFormCheck();
 
     $("#username").html("欢迎您! "+ decrypt(username));
+
+    //首页时间初始化
+    showTime();
 });
+
+function showTime() {
+    var now = new Date();
+    var nowTime = now.toLocaleString();
+    var date = nowTime.substring(0,10);//截取日期
+    var time = nowTime.substring(13,21); //截取时间
+    var week = now.getDay(); //星期
+    var hour = now.getHours(); //小时
+    //判断星期几
+    var weeks = ["日","一","二","三","四","五","六"];
+    var getWeek = "星期" + weeks[week];
+    var sc;
+    //判断是AM or PM
+    if(hour >= 0 && hour < 5){
+        sc = '凌晨';
+    }
+    else if(hour > 5 && hour <= 7){
+        sc = '早上';
+    }
+    else if(hour > 7 && hour < 11){
+        sc = '上午';
+    }
+    else if(hour >= 11 && hour <= 12){
+        sc = '中午';
+    }
+    else if(hour> 12 && hour <= 18){
+        sc = '下午';
+    }
+    else if(hour > 18 && hour <= 23){
+        sc = '晚上';
+    }
+    document.getElementById('time').innerHTML ="&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;" + date+" " + getWeek +" "+"  "+sc+""+time;
+    setTimeout('showTime()',1000);
+}
+
 function f_heightChanged(options)
 {
     if (tab)
